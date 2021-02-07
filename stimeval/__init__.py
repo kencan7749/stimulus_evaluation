2622da635c4d2cea3bb1a3a89964170ff345cf30
Ken <shirakawa.ken.38w@st.kyoto-u.ac.jp>
2021-02-07 22:46:14 +0900

    [mod] name change

--- stimeval/__init__.py
+++ stimeval/__init__.py
@@ -1,11 +1,9 @@
 """
-VideoEval: Quantitative evaluation generated/reconstructed video for Python3
-
+stimeval: Quantitative evaluation generated/reconstructed image/vide/dnn features for Python3
+Original repository is:
+https://github.com/kencan7749/stimulus_evaluation 
 Developed by Ken Shirakawa, Kamitani lab, Kyoto Univ.
 """
-
-#'import veval implicitly imports class `VidEval` (and package `VidEval`) and 
-# package `utils`.
 
 from .stimeval import *
 from .utils import * 
